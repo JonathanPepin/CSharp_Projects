38f480ab7fed874be07d84decf4ba83dfe7ad521
jpepin8 <jpepin8@gmail.com>
2020-05-21 15:26:27 -0400

    Minimized/optimized number of iterations to calcualte pi and not lose precision

--- Numbers/FindPiTotheNth.cs
+++ Numbers/FindPiTotheNth.cs
@@ -14,24 +14,24 @@ namespace Numbers
         static void Main(string[] args)
         {
             //using Archimedes method
-            double pi = 0;
-            double PolySides = 6;    //start by cutting circle with 6 triangles
-            double Sidelen = 1;      //start with 6 equilateral triangles of side = 1
-            double Dia = 2;          //Diameter of cricle or any polygon shape always 2
-            double NewSidelen;
-            double Perim;
-            double RadiusA;          //First part of radius calculation (middle to edge op polygon)
-            double RadiusB;          //Second part of radius calculation (egde of polygon to edge of circle)
+            double pi = 0;              //where to store calculated value of pi
+            double PolySides = 6;   //start by cutting circle with 6 triangles
+            double Sidelen = 1;     //start with 6 equilateral triangles of side = 1
+            double Dia = 2;         //Diameter of cricle or any polygon shape always 2
+            double NewSidelen;      //Calculates sidelen for next iteration
+            double Perim;           //Perimeter of polygon
+            double RadiusA;         //First part of radius calculation (middle to edge op polygon)
+            double RadiusB;         //Second part of radius calculation (egde of polygon to edge of circle)
 
-            while (PolySides < 18446744073709551615)  //while poly side smaller than maximum range of ulong
+            while (PolySides < 402653184)  // minimum size to have precision up to 16 digits
             {
                 RadiusA = Math.Sqrt(1 - Math.Pow(Sidelen / 2,2));
                 RadiusB = 1 - RadiusA;
                 NewSidelen = Math.Sqrt(Math.Pow(RadiusB, 2) + Math.Pow(Sidelen / 2, 2));
                 Perim = PolySides * Sidelen;
                 pi = Perim / Dia;
-                // Console.WriteLine("PolySides = " + PolySides + "\nSidelen = " + Sidelen + "\nRadiusA = " + RadiusA + "\nRadiusB = " + RadiusB + "\nPerim = " + Perim + "\nPi approx = " + pi + "\n");
-                // Console.ReadLine();
+                //Console.WriteLine("PolySides = " + PolySides + "\nSidelen = " + Sidelen + "\nRadiusA = " + RadiusA + "\nRadiusB = " + RadiusB + "\nPerim = " + Perim + "\nPi approx = " + pi + "\n");
+                //Console.ReadLine();
                 PolySides = PolySides * 2;
                 Sidelen = NewSidelen;
             }
